1cb4bf8625161a2c1b893ac38fb1252f9b57dcb8
tkellehe <mitchell.kelleher@gmail.com>
2019-08-04 17:50:25 -0400

    Adding Test Code
    Adding some test code and starting on the optimizedish matrix logic. Taking a lot longer than I thought...

--- src/js/detail/matrix.js
+++ src/js/detail/matrix.js
@@ -1,11 +1,27 @@
+//------------------------------------------------------------------------------------------------------------
+const makeArray = function makeArray_arglist() {
+    return new Float64Array(arguments);
+}
 
-function matrix(array, num_columns, num_rows) {
-    var self = this;
+//------------------------------------------------------------------------------------------------------------
+const Matrix = function MatrixRxC(array, num_rows, num_columns) {
+    let self = this;
     self.array = array;
-    self.num_columns = num_columns;
     self.num_rows = num_rows;
+    self.num_columns = num_columns;
+
+    self.access = function access_rc(r, c) {
+        return array[r*num_rows + c]
+    }
 
     self.multiply = function multiply(other) {
-        
+        let onum_columns = other.num_columns;
+        let onum_rows = other.num_rows;
+        let result = new MatrixRxC(new Float64Array());
+        for(let i = 0; i < num_rows; ++i) {
+            for(let j = 0; j < onum_columns; ++j) {
+                let off
+            }
+        }
     }
 }

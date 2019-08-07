8a418527238e785f467d5bebd23554f0c36d1ac9
tkellehe <mitchell.kelleher@gmail.com>
2019-08-07 10:09:31 -0400

    Update neural.js

--- src/js/detail/neural.js
+++ src/js/detail/neural.js
@@ -1,4 +1,8 @@
 //************************************************************************************************************
+const isLittleEndian = ((new Uint32Array((new Uint8Array([1,2,3,4])).buffer))[0] === 0x04030201)
+const isBigEndian = !isLittleEndian;
+
+//************************************************************************************************************
 const Layer = function(weights, biases, input_layer) {
     let self = this;
     self.weights = weights;

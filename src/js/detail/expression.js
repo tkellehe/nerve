85143c7aa01fc044da4c1524ef7964182ba9fe7d
tkellehe <mitchell.kelleher@gmail.com>
2019-08-08 12:28:58 -0400

    Update expression.js

--- src/js/detail/expression.js
+++ src/js/detail/expression.js
@@ -461,20 +461,44 @@ const Network = function(inputs, layers, outputs) {
     self.outputs = outputs;
     
     //--------------------------------------------------------------------------------------------------------
-    self.predict = function(input) {
+    self.input_to_tf = function(input) {
         const ins = this.inputs.collectors.length;
-        input = tf.tensor([this.inputs.uncollect(network_string_clense(input, ins), 0, 1)]);
-        let output = layers.predict(input);
+        return tf.tensor([this.inputs.uncollect(network_string_clense(input, ins), 0, 1)]);
+    }
+    
+    //--------------------------------------------------------------------------------------------------------
+    self.output_to_tf = function(output) {
+        const outs = this.outputs.collectors.length;
+        return tf.tensor([this.outputs.uncollect(network_string_clense(output, outs), 0, 1)]);
+    }
+    
+    //--------------------------------------------------------------------------------------------------------
+    self.predict = function(input) {
+        let output = layers.predict(this.input_to_tf(input));
         return this.outputs.collect(output.dataSync());
     }
 
     //--------------------------------------------------------------------------------------------------------
     self.learn = function(input, expected) {
         const outs = this.outputs.collectors.length;
-        const ins = this.layers.get_num_inputs();
-        input = tf.tensor([this.inputs.uncollect(network_string_clense(input, ins), 0, 1)]);
         expected = tf.tensor([this.outputs.uncollect(network_string_clense(expected, outs), 0, 1)]);
-        this.layers.learn(input, expected);
+        this.layers.learn(this.input_to_tf(input), this.output_to_tf(expected));
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+    self.batch = function(inputs, expecteds, num_batches=1) {
+        let tf_inputs = new Array(inputs.length);
+        let tf_expecteds = new Array(expecteds.length);
+        for(let i = 0, l = inputs.length; i < l; ++i) {
+            tf_inputs[i] = this.input_to_tf(inputs[i]);
+            tf_expecteds[i] = this.output_to_tf(expecteds[i]);
+        }
+        
+        for(let n = num_batches; n--;) {
+            for(let i = 0, l = inputs.length; i < l; ++i) {
+                this.layers.learn(tf_inputs[i], tf_expecteds[i]);
+            }
+        }
     }
     
     //--------------------------------------------------------------------------------------------------------

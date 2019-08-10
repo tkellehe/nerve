e0d641ace72086061ea6edcb899604d3b1d7ecc7
tkellehe <mitchell.kelleher@gmail.com>
2019-08-10 12:23:29 -0400

    Update nerve.js

---
+++ src/js/nerve.js
@@ -0,0 +1,12 @@
+nerve = {};
+nerve.execute_verbose = async function(code) {
+    global_network_memory_reset();
+    let compiled = eval("(function() { return " + code + "})()");
+    let result = await compiled.finalize();
+    
+    // Must reset the memory so that network can properly rebuild the memory expression.
+    global_network_memory_reset();
+    result.expression = network.to_expression();
+    network.destroy();
+    return result;
+}

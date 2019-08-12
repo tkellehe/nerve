518dd65271533774b345b1dc6b78501adf8d286a
tkellehe <mitchell.kelleher@gmail.com>
2019-08-12 13:21:57 -0400

    Update neural.js

--- src/js/detail/neural.js
+++ src/js/detail/neural.js
@@ -67,12 +67,17 @@ const Network = function(inputs, layers, outputs, info) {
             try {
                 var count = 0;
                 for(let n = num_batches; n--;) {
-                    let promise = tf_data.forEachAsync((data) => {
+                    tf_data.forEachAsync((data) => {
                         self.optimizer.minimize(() => {
                             return self.loss.apply(tf.losses, [data.expected, self.layers.predict(data.input), ...self.loss_args]);
                         });
-                    });
-                    promise.then(() => {
+                    }).catch((e) => {
+                        for(let i = 0, l = _tf_inputs.length; i < l; ++i) {
+                            tf.dispose(_tf_inputs[i]);
+                            tf.dispose(_tf_expecteds[i]);
+                        }
+                        reject(e)
+                    }).then(() => {
                         count += 1;
                         if(count === num_batches) {
                             for(let i = 0, l = _tf_inputs.length; i < l; ++i) {

dfd7c65f55a0bdc635af407ad6cf00d2d0757f7b
tkellehe <mitchell.kelleher@gmail.com>
2019-08-14 11:57:45 -0400

    Update nerve.js

--- src/js/nerve.js
+++ src/js/nerve.js
@@ -2,24 +2,24 @@ nerve = {};
 nerve.execute_verbose = async function(code) {
     global_network_memory_reset();
     let compiled = eval("(function() { return " + code + "})()");
-    compiled.finalize().then((result) => {
-        try {
-            // Must reset the memory so that network can properly rebuild the memory expression.
-            global_network_memory_reset();
-            result.expression = result.network.to_expression();
-
-            let memory = global_network_memory_to_expression();
-            if(memory.length) {
-                result.expression += ".memory(" + memory + ")";
-            }
-        } catch(e) {
-            result.network.destroy();
-            throw e;
+    let result = await compiled.finalize();
+    
+    try {
+        // Must reset the memory so that network can properly rebuild the memory expression.
+        global_network_memory_reset();
+        result.expression = result.network.to_expression();
+    
+        let memory = global_network_memory_to_expression();
+        if(memory.length) {
+            result.expression += ".memory(" + memory + ")";
         }
-
+    } catch(e) {
         result.network.destroy();
-        resolve(result);
-    });
+        throw e;
+    }
+    
+    result.network.destroy();
+    return result;
 }
 
 nerve.short_to_verbose = function(code) {

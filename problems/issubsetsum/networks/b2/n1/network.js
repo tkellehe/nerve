73a2dfc838e8472506d2dfc0287790b1f8c8d932
tkellehe <mitchell.kelleher@gmail.com>
2019-08-16 15:15:42 -0400

    Create network.js

---
+++ problems/issubsetsum/networks/b2/n1/network.js
@@ -0,0 +1,1 @@
+network(mapping(switchchar("1"),switchchar("1")),layers(layer.data(2,2)),mapping(switchchar("01"))).memory(string("%5B%A48%3F%8A%82%19%3E%00%7F7%3F%01%2C0%3E%E6%AE%5B%BC%AB%00%00%BD"))

39198833a4087cb696e0cfdc83ee325c088ea093
tkellehe <mitchell.kelleher@gmail.com>
2020-11-10 12:01:09 -0500

    Update find_primes.py

--- src/python/find_primes.py
+++ src/python/find_primes.py
@@ -2,4 +2,4 @@
 
 from tad import *
 settings.output_mode = OUTPUT_MODE_ARRAY
-search(RuleSetPrimes54(), 100000, step_to_output_limit=1000)
+search(RuleSetPrimes54(), 100910, step_to_output_limit=1000)

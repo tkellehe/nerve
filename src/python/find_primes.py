54ca9aa8bc1f12f3d6e114eb85b2dd9f3de445bd
tkellehe <mitchell.kelleher@gmail.com>
2020-11-10 19:53:34 -0500

    progress
    found a tad that produces 6 primes with only 4 bytes of information.

--- src/python/find_primes.py
+++ src/python/find_primes.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+# python3 -u find_primes.py | tee primes.txt
+
+# parser = TadParser()
+# parser.fromstring(encoding.frombytes([5, 162, 5, 2]))
+# parser.run()
+# print(parser.output) # => [97, 157, 43, 211, 109, 167]
+# # index @ 132514
+
 from tad import *
 settings.output_mode = OUTPUT_MODE_ARRAY
 # search(RuleSetPrimes54(), 100910, step_to_output_limit=1000)

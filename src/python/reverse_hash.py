7cab40f872391fe40d7b358684d6abae8130f2fd
tkellehe <mitchell.kelleher@gmail.com>
2020-11-20 20:36:02 -0500

    Update reverse_hash.py

--- src/python/reverse_hash.py
+++ src/python/reverse_hash.py
@@ -1,5 +1,6 @@
 from numpy import array, uint8, uint16, warnings
 from functools import partial
+import numpy as np
 
 # Turn of numpy warnings because we know we will overflow.
 warnings.filterwarnings('ignore')
@@ -58,39 +59,115 @@ primes = array([\
     233, 239, 241, 251\
 ], dtype=uint8)
 
-class Tid(object):
-    def __init__(self, p, x, y, c, i=None):
-        self.p = p
-        self.x = x
-        self.y = y
+class Constant(object):
+    def __init__(self, c):
         self.c = c
-        self.a = 0
-        self.i = (lambda: 0) if i is None else i
-        self.d = partial(d, n=pmmi[self.p])
     def __call__(self):
-        self.y = self.d(self.d(self.d(self.d(self.d(self.i())^self.c)^self.a)^self.x)^self.y)
-        self.a += 1
-        return self.y
+        return self.c
 
-import numpy as np
+def remove_last_occurance(l, v):
+    for i in range(len(l)-1, -1, -1):
+        if v == l[i]:
+            del l[i]
+            break
 
-# Tid(p=8, x=123, y=3, c=54) => produces primes within 835 numbers.
+class Pickah(object):
+    def __init__(self, p, c, k, H=1, i=None):
+        self.p = p
+        self.i = Constant(0) if i is None else i
+        self.c = c
+        self.k = k
+        self.a = 0
+        self.H = H
+        self._h = 0
+        self.h = np.zeros(H, dtype=uint8)
+        self.d = partial(d, n=pmmi[self.p])
+    def __repr__(self):
+        return repr((self.p, self.c, self.k, self.H))
+    def __str__(self):
+        return repr(self)
+    def __call__(self):
+        r = self.d(self.d(self.d(self.d(self.i())^self.c)^self.k)^self.a)
+        for h in self.h:
+            r = self.d(r^h)
+        self.a = uint8(self.a + 1)
+        if self.H:
+            self.h[self._h] = r
+            self._h = (self._h + 1) % self.H
+        return r
 
-def find(x0=0,xN=256,y0=0,yN=256,p=0,T=100):
+class Tid(object):
+    def __init__(self, k, pro, con, n=1):
+        self.k = k
+        self.pro = pro
+        self.pro.k = self.k
+        self.con = con
+        self.con.k = self.k
+        self.n = n
+        self.results = []
+    def __call__(self):
+        p = self.pro()
+        self.results.append(p)
+        for _ in range(self.n):
+            c = self.con()
+            remove_last_occurance(self.results, c)
+        if len(self.results) == self.k+1:
+            return self.results[0:self.k]
+    def r(self):
+        return self.results[0:self.k]
+    def run(self, T=None):
+        if T is None:
+            while self() is None:
+                pass
+        else:
+            for _ in range(T):
+                self()
+        return self.r()
+
+def find_primes(p0=0, pN=256, c0=0, cN=256, pp=0, pH=1, cp=0, cH=1, T=100, n=1):
     b = 0
-    bx = None
-    by = None
-    for x in range(x0,xN):
-        print(x)
-        for y in range(y0,yN):
-            t = Tid(p, x, y, len(primes))
-            o = array([t() for _ in range(T)], dtype=uint8)
-            c = np.count_nonzero(np.isin(primes, o))
-            if b < c:
-                b = c
-                bx = x
-                by = y
-                print(b, bx, by)
-                if c == t.c:
-                    return b, bx, by
-    return b, bx, by
+    bpc = None
+    bcc = None
+    search = primes[0:10]
+    for pc in range(p0, p0+pN):
+        print(pc)
+        pro = Pickah(p=pp, c=pc, k=len(search), H=pH)
+        for cc in range(c0, c0+cN):
+            con = Pickah(p=cp, c=cc, k=len(search), H=cH)
+            t = Tid(len(search), pro, con, n=n)
+            r = None
+            for _ in range(T):
+                r = t()
+                if r is not None:
+                    break
+            r = t()
+            z = np.count_nonzero(np.isin(search, r))
+            if b < z:
+                b = z
+                bpc = pc
+                bcc = cc
+                print(b, bpc, bcc)
+                if b == len(search):
+                    return b, bpc, bcc
+    return b, bpc, bcc
+
+# Maybe new concept is to produce neural networks from DNA like things...
+
+def find_pickah_with_most(l, is_ordered=False):
+    b = 0
+    bP = None
+    l = array(l)
+    for p in range(16):
+        for H in range(16):
+            print("    %i %i"%(p, H))
+            for c in range(256):
+                P = Pickah(p=p, c=c, H=H, k=len(l))
+                r = [P() for _ in range(len(l))]
+                z = np.count_nonzero((l == r) if is_ordered else np.isin(l, r))
+                if b < z:
+                    b = z
+                    bP = P
+                    print(b, bP)
+                    if len(l) == b:
+                        return b, bP
+    return b, bP

22f5322825e2ed084031689c0229c6e98374e9b8
tkellehe <mitchell.kelleher@gmail.com>
2024-01-16 23:10:24 -0500

    Update data.js

--- test/increasingByteSequence/data.js
+++ test/increasingByteSequence/data.js
@@ -1,16 +1,17 @@
 if (!window.increasingByteSequence) window.increasingByteSequence = {}
 
 window.increasingByteSequence.data = [
-  { input: [1, 2], output: [3, 4] },
-  { input: [3, 4], output: [5, 6] },
-  { input: [2, 3], output: [5, 7] },
-  { input: [5, 7], output: [7, 11] },
-  { input: [2, 4], output: [6, 8] },
-  { input: [6, 8], output: [10, 12] },
-  { input: [3, 6], output: [9, 12] },
-  { input: [9, 12], output: [15, 18] },
-  { input: [2, 4], output: [8, 16] },
-  { input: [5, 10], output: [15, 20] },
-  { input: [1, 1], output: [2, 3] },
-  { input: [2, 3], output: [5, 8] }
+  '1 2 3 4',
+  '3 4 5 6',
+  '5 7 11 13',
+  '2 4 6 8',
+  '6 8 10 12',
+  '3 6 9 12',
+  '9 12 15 18',
+  '2 4 8 16',
+  '5 10 15 20',
+  '1 1 2 3',
+  '2 3 5 8',
+  '9 10 11 12',
+  '11 13 17 19'
 ]
